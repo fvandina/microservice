bf6224328865ffe85c4c3a2455dcc42d10e3d3c4
Luis Guzman <lguzman@fvecuador.com>
2023-02-08 16:34:40 -0500

    Part one RabbitMQ

--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -1,14 +1,35 @@
 package com.amigoscode.notification;
 
+import com.amigoscode.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.amigoscode.notification",
+                "com.amigoscode.amqp"
+        }
+)
 @EnableDiscoveryClient
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    @Bean
+     CommandLineRunner commandLineRunner (
+             RabbitMQMessageProducer producer,
+             NotificationConfig notificationConfig){
+        return args -> {
+            producer.publish(
+                    "foo",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey()
+            );
+        };
+     }
 }
