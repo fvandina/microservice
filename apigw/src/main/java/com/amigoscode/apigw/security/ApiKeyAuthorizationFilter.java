afca281af660d52bbf046efe67795328c2e8447a
Luis Guzman <lguzman@fvecuador.com>
2023-02-17 07:42:25 -0500

    Security - API Key Authentication

---
+++ apigw/src/main/java/com/amigoscode/apigw/security/ApiKeyAuthorizationFilter.java
@@ -0,0 +1,46 @@
+package com.amigoscode.apigw.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class ApiKeyAuthorizationFilter implements GlobalFilter, Ordered {
+
+    private final FakeApiAuthorizationChecker fakeApiAuthorizationChecker;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("ApiKeyAuthorizationFilter ... checking the key");
+        Route attribute = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+        String application = attribute.getId();
+
+        List<String> apiKey = exchange.getRequest().getHeaders().get("ApiKey");
+
+        String key = apiKey.get(0);
+        if (application == null
+                || (key == null || key.isEmpty())
+                || !fakeApiAuthorizationChecker.isAuthorized(key, application)
+        ) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You are not authorized");
+        }
+        System.out.println(key);
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
